bfaa6e604bf67eed36123a7161889b3de28c0e88
nastushalk <nastusha.leve@gmail.com>
2021-02-19 23:44:56 +0200

    build: update deps and fix tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,21 +5,26 @@ describe('epuppy', () => {
   describe('generateLink', () => {
     test('beta', () => {
       const result = generateLink(fixtures.beta)
-      expect(result).toEqual(
-        'https://electronjs.org/releases/beta#9.0.0-beta.24'
-      )
+      expect(result).toEqual({
+        link: 'https://electronjs.org/releases/beta#9.0.0-beta.24',
+        channel: 'beta',
+      })
     })
 
     test('nighlty', () => {
       const result = generateLink(fixtures.nighlty)
-      expect(result).toEqual(
-        'https://electronjs.org/releases/nightly#10.0.0-nightly.20200520'
-      )
+      expect(result).toEqual({
+        link: 'https://electronjs.org/releases/nightly#10.0.0-nightly.20200520',
+        channel: 'nightly',
+      })
     })
 
     test('stable', () => {
       const result = generateLink(fixtures.stable)
-      expect(result).toEqual('https://electronjs.org/releases/stable#9.0.0')
+      expect(result).toEqual({
+        link: 'https://electronjs.org/releases/stable#9.0.0',
+        channel: 'stable',
+      })
     })
   })
 
