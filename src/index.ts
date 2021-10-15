c26ba95bed0d658f0f58dd7269900d24565a31ea
sokrox <sokrox@outlook.com>
2021-10-15 16:00:42 +0300

    skip this

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ const TWIT_NIGHTLY = process.env.TWIT_NIGHTLY || true
 
 const foundNewVersion = async () => {
   const verInPackage = pkg.dependencies['mini-electron-releases']
+  // @ts-expect-error
   const verOnNPM = (
     await axios.get('https://registry.npmjs.com/mini-electron-releases', {
       responseType: 'json',
